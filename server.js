ff991e997db562a6cc44f4fc2efdd15408a102a9
mdarf <mdarfinji45@gmail.com>
2024-01-20 15:32:31 +0800

    added booking listing user scheema

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.static("public"));
 const PORT = 3001;
 mongoose
   .connect(process.env.MONGO_URL, {
-    dbName: "Dream_Nest",
+    dbName: "Buy-sell",
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
